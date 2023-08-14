b4a3a9cd8099f9176665393ef06c6db24646763c
thien <thien.nhm92@gmail.com>
2023-08-14 22:32:39 +0700

    init

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = ' '
+
+local keymap = vim.keymap
+
+
+-- Diagnostic keymaps
+keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
---
+++ lua/core/options.lua
@@ -0,0 +1,45 @@
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+
+local o = vim.o
+local wo = vim.wo
+
+-- Set highlight on search
+o.hlsearch = true
+
+-- Make line numbers default
+wo.number = true
+
+-- Enable mouse mode
+o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+o.breakindent = true
+
+-- Save undo history
+o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Keep signcolumn on by default
+wo.signcolumn = 'yes'
+
+-- Decrease update time
+o.updatetime = 250
+o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+o.termguicolors = true
+
